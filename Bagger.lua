4e106cb2e890c9b586b4a06e263f5bba2d106092
Vebjørn <vBaustad@users.noreply.github.com>
2020-10-18 01:33:36 +0200

    cleaned up folders, tested "OnEvent" trigger

--- Bagger.lua
+++ Bagger.lua
@@ -7,3 +7,25 @@
 local L = BaggerLocalization
 
 print(L.FRAMES_LOCKED)
+
+
+
+--Print to chat when entering/leaving combat
+if not MyCombatFrame then
+    CreateFrame("Frame", "MyCombatFrame", UIParent)
+end
+
+MyCombatFrame:RegisterEvent("PLAYER_REGEN_ENABLED")
+MyCombatFrame:RegisterEvent("PLAYER_REGEN_DISABLED")
+
+function MyCombatFrame_OnEvent(self, event, ...)
+    if event == "PLAYER_REGEN_ENABLED" then
+        print("Leaving Combat...")
+    elseif event == "PLAYER_REGEN_DISABLED" then
+        print("Entering Combat!")
+    end
+end
+
+MyCombatFrame:SetScript("OnEvent", MyCombatFrame_OnEvent)
+
+
