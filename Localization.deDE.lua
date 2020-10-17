a23bf7d69dc536e68355e6efb128ad5155a7411c
Vebjørn <vBaustad@users.noreply.github.com>
2020-10-18 01:06:59 +0200

    add initial Localization, split test file and prod

---
+++ Localization.deDE.lua
@@ -0,0 +1,9 @@
+
+if GetLocale() == "deDE" then
+    if not BaggerLocalization then
+        BaggerLocalization = {}
+    end
+    
+    BaggerLocalization["FRAMES_LOCKED"] = "Frames wurden gesperrt"
+    BaggerLocalization["FRAMES_UNLOCKED"] = "Frames wurden entsperrt"
+end
---
+++ Localization.enUS.lua
@@ -0,0 +1,4 @@
+BaggerLocalization = {}
+
+BaggerLocalization["FRAMES_LOCKED"] = "Frames have been locked"
+BaggerLocalization["FRAMES_UNLOCKED"] = "Frames have been unlocked"
