34e0cda0be791490b110983a60c76835bbcd90bd
Vebjørn <vBaustad@users.noreply.github.com>
2020-10-29 01:15:57 +0100

    adding items to bag, sorting and quality color

--- BagChamp.lua
+++ BagChamp.lua
@@ -51,15 +51,57 @@
 
 -- Set variables
 local L = BagChampLocalization
-local bagslots = 24
 local ItemSlotsWidth = 12
 local iconSpaceHeight = 7
-local iconSpaceWidth = 12
+local iconSpaceWidth = 7
 local buttonsize = 35
 
 
+
+
+
+
+
+
+
+
+--Calculate height of the bag
+local function BagChamp_CalculateHeight(bagslots, width, spacing, buttonsize)
+
+    rows = math.ceil(bagslots / width)
+    return ( rows * buttonsize) + (rows * spacing) +60
+
+end
+
+-- Calculate width of the bag
+local function BagChamp_CalculateWidth(bagslots, width, spacing, buttonsize) 
+
+    return (width * buttonsize) + (width * spacing) + 14
+
+end
+
+local function itemNameSort(a, b)
+    return a.name < b.name
+end
+
 --Calculate bagslots
 function BagChamp_CalculateBagslots()
+    
+    local freeSlots = 0
+    local totalSlots = 0
+
+    for bag = 0, NUM_BAG_SLOTS do
+        totalSlots = totalSlots + GetContainerNumSlots(bag)
+        for slot = 1, GetContainerNumSlots(bag) do
+            local texture = GetContainerItemInfo(bag, slot)            
+            if not texture then
+                freeSlots = freeSlots +1
+            end
+        end
+    end
+
+    
+    print("You have", totalSlots, "slots total in your bags, and", freeSlots, " free slots in your inventory")
 
     --get number of bagslots from window
 
@@ -71,74 +113,109 @@ function BagChamp_CalculateBagslots()
 
     -- Return used slots, free slots, minimalist option true og false
 
-    var1 = 1
-    var2 = 2
-    var3 = true
-
-    return var1, var2, var3
-
-end
-
---Calculate height of the bag
-function BagChamp_CalculateHeight(bagslots, width, spacing, buttonsize)
-
-    rows = math.ceil(bagslots / width)
-    return ( rows * buttonsize) + (rows * spacing) +70
-
-end
-
--- Calculate width of the bag
-function BagChamp_CalculateWidth(bagslots, width, spacing, buttonsize) 
-
-    return (width * buttonsize) + (width * spacing) + 40
+    return totalSlots, freeSlots
 
 end
 
 --On Load function
 function BagChamp_OnLoad(self)
-
-
+        
     --testing assigning multiple variables from one call
-    var1, var2, var3 = BagChamp_CalculateBagslots()
+    local totalSlots, freeSlots = BagChamp_CalculateBagslots()   
+    
+    local width = BagChamp_CalculateWidth(totalSlots, ItemSlotsWidth, iconSpaceWidth, buttonsize)
+    local height = BagChamp_CalculateHeight(totalSlots, ItemSlotsWidth, iconSpaceHeight, buttonsize)
 
-    print(var1 .. " + " .. var2 .. " ")
-    
-    local width = BagChamp_CalculateWidth(bagslots, ItemSlotsWidth, iconSpaceWidth, buttonsize)
-    local height = BagChamp_CalculateHeight(bagslots, ItemSlotsWidth, iconSpaceHeight, buttonsize)
-    
-   
     --Set frame size  
     self:SetSize(width, height)
 
     --Create the item slots
     itemWidth = 1
     self.items = {}
-    for idx = 1, bagslots do 
+    for idx = 1, totalSlots do 
         local item = CreateFrame("Button", "BagChamp_Item" .. idx, self, "BagChampItemTemplate")       
 
         self.items[idx] = item
         if idx == 1 then
-            item:SetPoint("TOPLEFT", 30, -50)
+            item:SetPoint("TOPLEFT", 10, -50)
             itemWidth = itemWidth +1
         elseif itemWidth == ItemSlotsWidth+1 then
             item:SetPoint("TOPLEFT", self.items[idx- ItemSlotsWidth], "BOTTOMLEFT", 0, -7)
             itemWidth = 2
         else 
-            item:SetPoint("TOPLEFT", self.items[idx-1], "TOPRIGHT", 12, 0)
+            item:SetPoint("TOPLEFT", self.items[idx-1], "TOPRIGHT", 7, 0)
             itemWidth = itemWidth +1            
         end
     end 
-
 end
 
 
+function BagChamp_Update()
+
+    local totalSlots, freeSlots = BagChamp_CalculateBagslots() 
+
+    local items = {}
+    -- Scan through the bag slots, looking for items
+    for bag = 0, NUM_BAG_SLOTS do
+        for slot = 0, GetContainerNumSlots(bag) do
+            local texture, count, locked, quality, readable, lootable, link = GetContainerItemInfo(bag, slot)
+            if texture then
+                -- If found, grab the item number and store other data
+                local itemNum = tonumber(link:match("|Hitem:(%d+):"))
+                if not items[itemNum] then
+                    items[itemNum] = {
+                    texture = texture,
+                    count = count,
+                    quality = quality,
+                    name = GetItemInfo(link),
+                    link = link,
+                    }
+                else
+                    -- The item already exists in our table, just update the count
+                    items[itemNum].count = items[itemNum].count + count
+                end
+            end
+        end
+    end
+
+    --Sort items in table
+    local sortTbl = {}
+    for link, entry in pairs(items) do
+        table.insert(sortTbl, entry) 
+    end
+    table.sort(sortTbl, itemNameSort)
 
 
+    --Display items in bag (In order)
+    for i = 1, totalSlots do
+        local button = BagChamp.items[i]
+        local entry = sortTbl[i]
 
+        if entry then
+            --There is an item in this slot
 
+            button.icon:SetTexture(entry.texture)
+            if entry.count > 1 then
+                button.count:SetText(entry.count)
+                button.count:Show()
+            else
+                button.count:Hide()
+            end
 
+            if entry.quality > -1 then 
+                button.glow:SetVertexColor(GetItemQualityColor(4))
+                --button.glow:SetVertexColor(GetItemQualityColor(entry.quality))
+                button.glow:Show()
+            else
+                button.glow:Hide()
+            end
+            button:Show()
+        else
+            --button:Hide()
+        end 
+    end
 
-
+end
 
 
 
